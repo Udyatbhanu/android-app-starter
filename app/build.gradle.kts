73e004b02fca2f4d434cbe71a51d3e1746a54241
skillmuffin <bhanu.baliyar@gmail.com>
2025-04-04 09:06:12 -0700

    Fix gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,16 +30,6 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
-    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
